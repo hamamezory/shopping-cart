429b531faf9b29c719edbe1a0731cb3c3322f83a
hamamezory <hamamezory@gmail.com>
2021-06-26 14:20:37 +0300

    final deployment

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,21 @@ const app = express();
 
 app.use(express.json());
 
-//app.get("/", (req, res) => {
-  //res.json({ message: "API running..." });
-//});
 
-app.use("/api/products", productRoutes);
+if(process.env.NODE_ENV ==="Production")
+{
+  app.use(express.static(path.join(__dirname,'/interface/public')))
+  app.use("/api/products", productRoutes);
+  app.get('*', (req, res) => {
+res.sendfile(path.join(__dirname,'interface', 'public', 'index.html'));
+
+  } )
+}
+else{
+  app.get("/", (req, res) => {
+    res.json({ message: "API running..." });
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
