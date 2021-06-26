60dc3028bd1e1bd7c0e3a96d55bff55a34bb5785
hamamezory <36791257+hamamezory@users.noreply.github.com>
2021-06-26 22:44:01 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,33 @@ app.get("/", (req, res) => {
   res.json({ message: "API running..." });
 });
 
-app.use("/api/products", productRoutes); 
+app.use("/api/products", productRoutes);
 
-
+// --> Add this
+// ** MIDDLEWARE ** //
+const whitelist = ['http://localhost:3000', 'http://localhost:5000', 'https://shopping-cart1989.herokuapp.com']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+//end add this
+// --> Add this
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'interface/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'interface/build', 'index.html'));
+  });
+}
+//end add this
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
