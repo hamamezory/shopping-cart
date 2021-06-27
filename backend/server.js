d252fb11686e587f8cc2260c4835844bb14052bb
hamamezory <36791257+hamamezory@users.noreply.github.com>
2021-06-27 21:45:18 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
   res.json({ message: "API running..." });
 });*/
 
-//app.use("/api/products", productRoutes);
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 // --> Add this
