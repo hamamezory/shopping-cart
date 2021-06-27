45096e245a44e5d46598f569c725d8d9049c4a5d
hamamezory <36791257+hamamezory@users.noreply.github.com>
2021-06-27 22:54:29 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const productRoutes = require("./routes/productRoutes");
 const connectDB = require("./config/db");
-//new code
-var path = require('path');
-//end new code
+
 connectDB();
 
 const app = express();
@@ -14,20 +12,18 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "API running..." });
 });
-//new code
-app.use('/interface', express.static(path.join(__dirname, '/interface')));
-//end new code
 
-/*// --> Add this
+
+// --> Add this
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, '/interface/build')));
+  app.use(express.static(path.join(__dirname, '../interface/build')));
 // Handle React routing, return all requests to React app
   app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, '/interface/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../interface/build', 'index.html'));
   });
 }
-//end add this*/
+//end add this
 app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
