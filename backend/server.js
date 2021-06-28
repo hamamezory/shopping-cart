008ceed271e946f71e1bb801259eeda9c30a10d2
hamamezory <hamamezory@gmail.com>
2021-06-28 18:03:27 +0300

    second commit

--- backend/server.js
+++ backend/server.js
@@ -9,21 +9,11 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.json({ message: "API running..." });
+});
 
-if(process.env.NODE_ENV ==="Production")
-{
-  app.use(express.static(path.join(__dirname,'/interface/public')))
-  app.use("/api/products", productRoutes);
-  app.get('*', (req, res) => {
-res.sendfile(path.join(__dirname,'interface', 'public', 'index.html'));
-
-  } )
-}
-else{
-  app.get("/", (req, res) => {
-    res.json({ message: "API running..." });
-  });
-}
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
