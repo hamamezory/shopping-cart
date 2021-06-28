cf408c5f782573127288838085aa0a98324a32fc
hamamezory <36791257+hamamezory@users.noreply.github.com>
2021-06-28 16:15:45 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 //end add this
-app.use("/api/products", productRoutes);
+//app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
