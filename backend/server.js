ef767d28c840a6548419b76454ae66ea416533de
hamamezory <hamamezory@gmail.com>
2021-06-29 00:16:57 +0300

    second commit

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
-
+/*//step3
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('interface/build'))};
+//end step 3*/
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
